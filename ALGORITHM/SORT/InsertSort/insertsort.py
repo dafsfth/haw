ba11cb343adb084a27c9035a06973d784e6d65e0
dafsfth <2419553919@qq.com>
2020-08-15 19:43:30 +0800

    20.8.15, insertsort

---
+++ ALGORITHM/SORT/InsertSort/insertsort.py
@@ -0,0 +1,25 @@
+def insert_sort(A):
+    for i in range(1, len(A)):
+        x = A[i]
+        j = i - 1
+        while j >= 0 and A[j] > x:
+            A[j+1] = A[j]  # ∫Û“∆
+            j -= 1
+        A[j+1] = x
+
+
+Al = [10, 3, 2, 14, 23, 34, 9, 5, 7, 9, 3, 10]
+insert_sort(Al)
+print(Al)
+
+
+def sort(A):
+    max_list = []
+    for i in range(len(A)):
+        x = A.pop(A.index(max(A)))
+        max_list.append(x)
+    return max_list
+
+
+# a = sort(Al)
+# print(a)
